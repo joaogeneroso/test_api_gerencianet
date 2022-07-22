1ceddf84563cff5efdd7e92441617ba05bdeca7c
Joao Generoso <100031121+joaogeneroso@users.noreply.github.com>
2022-07-22 11:10:38 -0300

    Update credentials.py

--- app/credentials.py
+++ app/credentials.py
@@ -2,7 +2,7 @@
 
 # Put your app credentials here
 CREDENTIALS = {
-    'client_id': 'Client_Id_98feb8e4aa192c7d6badc5efdbdec83fa03658ae',
-    'client_secret': 'Client_Secret_8d7bd22e1432364e8759f93a549ec3edf00f3a72',
+    'client_id': 'Digite sua Client_Id',
+    'client_secret': 'Digite sua Client_Secret',
     'sandbox': True  # True para desenvolvimento e False para produção
 }
